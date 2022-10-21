eada42cedde926ce03c97652fab2b7d9e9228050
Annumit <115851650+Annumit@users.noreply.github.com>
2022-10-21 15:18:10 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 17737898))
+    API_HASH = os.environ.get("API_HASH", "ad762fe0516f367115ba651d929cf429")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5657801755:AAFASLmkl6diuREYdpPfAHVIh9Swkoc8Dsw")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1001654604022))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "MdiskSearchv1bot")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "5076949930"))
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", anumitultrabots)
     ABOUT_BOT_TEXT = """<b>This is Mdisk Search Bot.
 
 🤖 My Name: <a href='https://t.me/cyniteofficial'>Mdisk Search Robot</a>
