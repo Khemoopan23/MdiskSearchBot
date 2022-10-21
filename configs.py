52f0ef03ce2d60ede070e779c84f649fb00fdf33
Annumit <115851650+Annumit@users.noreply.github.com>
2022-10-21 15:21:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "ad762fe0516f367115ba651d929cf429")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5657801755:AAFASLmkl6diuREYdpPfAHVIh9Swkoc8Dsw")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchBot")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQCoADjfDF5RqE0rEiSQFyqGaKoTpxLv-Sdm2a-gq-PV7YS--hN3PmBvgTo88qwuCJxjxfDV7oW6DMW9ti4qED_-Lso9X-WhdwPi3MAEQ4MjG1e2ZwKOXYgiTefUQ77asGDQkYSvEKKrgerR_2PE1jhft0Rt6uc4PskQqkxEEAmUgclcvZOhg2-Lg_FHm3YmfSlo_n_LC-SAx8ERuv54kSWLZQbgXjnVIgjXurXpZYIF4a-Dx8PXeCxYc9uOtMHKfomMM5Qt78aJp5DL6Rvqy0_wMj4DfI2m32HjZMT1v-p1TJnCR4OnUiZtqTvQvvb264X9Tq3ER6O-w8d8-uBnRMMsAAAAAUKBF3YA")
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1001654604022))
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "MdiskSearchv1bot")
     BOT_OWNER = int(os.environ.get("BOT_OWNER", "5076949930"))
