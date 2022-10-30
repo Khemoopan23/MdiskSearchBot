426b2dd804fc9989c5b13583178b72ddbbce3ef6
Vikash Kumar <116530215+anumitultrabots@users.noreply.github.com>
2022-10-30 22:08:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,18 +1,18 @@
-# (c) @RoyalKrrishna
+# (c) 
 
 import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 17737898))
-    API_HASH = os.environ.get("API_HASH", "ad762fe0516f367115ba651d929cf429")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5657801755:AAFASLmkl6diuREYdpPfAHVIh9Swkoc8Dsw")
-    BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchBot")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQCoADjfDF5RqE0rEiSQFyqGaKoTpxLv-Sdm2a-gq-PV7YS--hN3PmBvgTo88qwuCJxjxfDV7oW6DMW9ti4qED_-Lso9X-WhdwPi3MAEQ4MjG1e2ZwKOXYgiTefUQ77asGDQkYSvEKKrgerR_2PE1jhft0Rt6uc4PskQqkxEEAmUgclcvZOhg2-Lg_FHm3YmfSlo_n_LC-SAx8ERuv54kSWLZQbgXjnVIgjXurXpZYIF4a-Dx8PXeCxYc9uOtMHKfomMM5Qt78aJp5DL6Rvqy0_wMj4DfI2m32HjZMT1v-p1TJnCR4OnUiZtqTvQvvb264X9Tq3ER6O-w8d8-uBnRMMsAAAAAUKBF3YA")
+    API_ID = int(os.environ.get("API_ID", ))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1001654604022))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "MdiskSearchv1bot")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", "5076949930"))
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001666254490")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
     ABOUT_BOT_TEXT = """<b>This is Mdisk Search Bot.
 
 🤖 My Name: <a href='https://t.me/anumitultrabots'>Mdisk Search Robot</a>
